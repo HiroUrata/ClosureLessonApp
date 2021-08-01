839f69e24c764540f6b8124c7803ebdc71749cb9
HiroUrata <1712026@i.softbank.jp>
2021-08-01 15:53:36 +0900

    Initial Commit

---
+++ ClosureLessonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClosureLessonApp
+//
+//  Created by UrataHiroki on 2021/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
